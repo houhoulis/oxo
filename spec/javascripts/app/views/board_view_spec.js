c6829b76c42420e170cf6d7ac6fef5ff4450a719
Chris Houhoulis <chris@chrishouhoulis.com>
2013-05-26 09:52:24 -0400

    Test that creating the Board adds to the DOM.

--- spec/javascripts/app/views/board_view_spec.js
+++ spec/javascripts/app/views/board_view_spec.js
@@ -1,19 +1,23 @@
 describe("App.Views.Board", function() {
-  
+
   beforeEach(function() {
     this.view = new App.Views.Board();
   });
-  
+
   describe("Instantiation", function() {
-    
-    it("should create a list element", function() {
+
+    it("should create a div element", function() {
       expect(this.view.el.nodeName).toEqual("DIV");
     });
-    
-    // it("should create a class", function() {
-    //   expect($(this.view.el)).toHaveId('oxo-board');
-    // });
-    
+
+    it("creates a <p> in the DOM", function() {
+      debugger;
+      expect($(this.view.el).has('p').length).toBeTruthy;
+    });
+
+    it("fails because it doesn't create a span in the DOM", function() {
+      expect($(this.view.el).has('span').length).toBeTruthy;
+    });
   });
-  
+
 });
